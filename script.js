575d5e5776152960f59486626b57e8bdf0868651
SarahTCummings <stc@umumvid.io>
2017-11-01 16:58:49 -0400

    timer

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ $(document).ready(function() {
   var lastClicked = "";
 
 
+
   $("#startButton").click(function() {
-    // flip the view when you start the game
+    var seconds_left = 10;
+    var interval = setInterval(function() {
+    document.getElementById('timer_div').innerHTML = --seconds_left;
+
+    if (seconds_left <= 0)
+    {
+       document.getElementById('timer_div').innerHTML = "You have lost.";
+       clearInterval(interval);
+    }
+}, 1000);
+         // flip the view when you start the game
     console.log("Button clicked");
     $("#startScreen").fadeToggle(function() {
       $("#startScreen").css("display", "none");
